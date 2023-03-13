64774ad471f8efd1b3e985afb1a7e65ee0f6f49a
koolkishan <kishansheth21@gmail.com>
2023-03-13 23:44:36 +0530

    Added Social Icons

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -1,7 +1,32 @@
+import Link from "next/link";
 import React from "react";
+import {
+  FiGithub,
+  FiInstagram,
+  FiYoutube,
+  FiLinkedin,
+  FiTwitter,
+} from "react-icons/fi";
 
 function SocialIcons() {
-  return <div>SocialIcons</div>;
+  const socialLinks = [
+    { name: "Github", icon: <FiGithub />, link: "https://www.github.com" },
+    { name: "Youtube", icon: <FiYoutube />, link: "" },
+    { name: "LinkedIn", icon: <FiLinkedin />, link: "" },
+    { name: "Instagram", icon: <FiInstagram />, link: "" },
+    { name: "Twitter", icon: <FiTwitter />, link: "" },
+  ];
+  return (
+    <div>
+      <ul>
+        {socialLinks.map(({ name, icon, link }) => (
+          <li key={name} title={name}>
+            <Link href={link}>{icon}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default SocialIcons;
