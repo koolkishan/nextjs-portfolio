ad26ed7a692e7a483630f562a3b186edc9a288a3
koolkishan <kishansheth21@gmail.com>
2023-03-14 13:18:35 +0530

    Completed the social icons component

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -11,17 +11,39 @@ import {
 function SocialIcons() {
   const socialLinks = [
     { name: "Github", icon: <FiGithub />, link: "https://www.github.com" },
-    { name: "Youtube", icon: <FiYoutube />, link: "" },
-    { name: "LinkedIn", icon: <FiLinkedin />, link: "" },
-    { name: "Instagram", icon: <FiInstagram />, link: "" },
-    { name: "Twitter", icon: <FiTwitter />, link: "" },
+    {
+      name: "Youtube",
+      icon: <FiYoutube />,
+      link: "https://www.youtube.com/KishanSheth21/",
+    },
+    {
+      name: "LinkedIn",
+      icon: <FiLinkedin />,
+      link: "https://www.linkedin.com/in/koolkishan/",
+    },
+    {
+      name: "Instagram",
+      icon: <FiInstagram />,
+      link: "https://instagram.com/koolkishansheth",
+    },
+    {
+      name: "Twitter",
+      icon: <FiTwitter />,
+      link: "https://twitter.com/koolkishansheth",
+    },
   ];
   return (
-    <div>
-      <ul>
+    <div className="social-icons">
+      <ul className="social-icons-list">
         {socialLinks.map(({ name, icon, link }) => (
-          <li key={name} title={name}>
-            <Link href={link}>{icon}</Link>
+          <li key={name} title={name} className="social-icons-list-item">
+            <Link
+              href={link}
+              className="social-icons-list-item-link"
+              target="_blank"
+            >
+              {icon}
+            </Link>
           </li>
         ))}
       </ul>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Email from "@/components/Email";
+import SocialIcons from "@/components/SocialIcons";
 import React from "react";
 
 function index() {
   return (
     <div className="app">
+      <SocialIcons />
       <Email />
     </div>
   );
