e13cf1f000997079fe853cdc1fb7880e0041b3fc
koolkishan <kishansheth21@gmail.com>
2023-03-14 13:06:50 +0530

    Chnages realted to scss setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/scss/globals.css";
+import "@/scss/index.scss";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
