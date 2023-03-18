6f122a851f0941f720c1ab13b0c916704def6bd4
koolkishan <kishansheth21@gmail.com>
2023-03-18 12:48:12 +0530

    Added fonts from next-fonts.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,24 @@
 import "@/scss/globals.css";
 import "@/scss/index.scss";
 import type { AppProps } from "next/app";
+import { Raleway, Fira_Code } from "next/font/google";
+
+const raleway = Raleway({ subsets: ["latin"] });
+const firaCode = Fira_Code({
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        :root {
+          --raleway: ${raleway.style.fontFamily};
+          --fira-code: ${firaCode.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />;
+    </>
+  );
 }
