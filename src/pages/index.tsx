426f86673832758740fc71788407132652d309ce
koolkishan <kishansheth21@gmail.com>
2023-03-20 12:00:00 +0530

    Added loader with framer motion animations.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,27 +5,40 @@ import SocialIcons from "@/components/SocialIcons";
 import Footer from "@/sections/Footer";
 import Hero from "@/sections/Hero";
 import Navbar from "@/sections/Navbar";
-import React from "react";
+import React, { useState } from "react";
 import About from "@/sections/About";
 import Contact from "@/sections/Contact";
+import { AnimatePresence } from "framer-motion";
 
-function index() {
+function Index() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [showContent, setShowContent] = useState(false);
+
+  const handleLoaderLoaded = () => {
+    setIsLoading(false);
+    setTimeout(() => setShowContent(true), 450);
+  };
+
   return (
     <div className="app">
       <Head>
         <title>Kishan Sheth</title>
         <link rel="shortcut icon" href="/favicon.png" />
       </Head>
-      <Navbar />
-      <SocialIcons />
-      <Email />
-      <Hero />
-      <About />
-      <Contact />
-      <Footer />
-      <Loader />
+      {showContent && (
+        <>
+          <Navbar />
+          <SocialIcons />
+          <Email />
+          <Hero />
+          <About />
+          <Contact />
+          <Footer />
+        </>
+      )}
+      <Loader isLoading={isLoading} setIsLoading={handleLoaderLoaded} />
     </div>
   );
 }
 
-export default index;
+export default Index;
