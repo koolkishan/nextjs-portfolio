425769c788576ef51aa9261266b2e9d146f9b3b0
koolkishan <kishansheth21@gmail.com>
2023-03-25 14:34:04 +0530

    Added navbar scroll blur and fixed effect

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,9 +1,10 @@
 import Button from "@/components/Button";
 import Logo from "@/components/Logo";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Navbar() {
+  const [navbarVisible, setNavbarVisible] = useState(false);
   const sectionLinks = [
     { name: "About", link: "/#about" },
     { name: "Experience", link: "/#experience" },
@@ -13,23 +14,34 @@ function Navbar() {
       link: "/#contact",
     },
   ];
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.pageYOffset > 100
+        ? setNavbarVisible(true)
+        : setNavbarVisible(false);
+    });
+  }, []);
+
   return (
     <nav>
-      <div className="brand">
-        <Link href="kishansheth.com">
-          <Logo />
-        </Link>
-      </div>
-      <div className="nav-items">
-        <ul className="nav-items-list">
-          {sectionLinks.map(({ name, link }) => (
-            <li key={name} className="nav-items-list-item">
-              <Link href={link}>{name}</Link>
-            </li>
-          ))}
-        </ul>
-        <div className="nav-items-button">
-          <Button text="Resume" link="http://localhost:3000/resume.pdf" />
+      <div className={`wrapper ${navbarVisible ? "blur-nav" : ""}`}>
+        <div className="brand">
+          <Link href="kishansheth.com">
+            <Logo />
+          </Link>
+        </div>
+        <div className="nav-items">
+          <ul className="nav-items-list">
+            {sectionLinks.map(({ name, link }) => (
+              <li key={name} className="nav-items-list-item">
+                <Link href={link}>{name}</Link>
+              </li>
+            ))}
+          </ul>
+          <div className="nav-items-button">
+            <Button text="Resume" link="http://localhost:3000/resume.pdf" />
+          </div>
         </div>
       </div>
     </nav>
